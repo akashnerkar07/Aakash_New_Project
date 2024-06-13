5f7d3e9dd6f61579c0ab178455476503859fd3e0
akashn7788 <akashn7788@gmail.com>
2024-06-13 13:51:39 +0530

    first commit

---
+++ Aakash_Project_New/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Aakash_Project_New project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aakash_Project_New.settings')
+
+application = get_asgi_application()
